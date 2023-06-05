18e660f783c651765b1688eeef162fdb41afbf98
Manhhoangvp95 <87350762+Manhhoangvp95@users.noreply.github.com>
2023-06-05 14:28:00 +0700

    Fix as last comment

--- starter/producers/connector.py
+++ starter/producers/connector.py
@@ -37,7 +37,7 @@ def configure_connector():
                 "table.whitelist": CONNECTOR_NAME,
                 "mode": "incrementing",
                 "incrementing.column.name": "stop_id",
-                "topic.prefix": "org.chicago.cta",
+                "topic.prefix": "org.chicago.cta.",
                 "poll.interval.ms": "60000",
             }
         }),
